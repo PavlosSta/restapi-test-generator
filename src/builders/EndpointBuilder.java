828c317d3e56881b02b6a2562c4068f3b1d32d7e
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-04-03 01:45:11 +0300

    Added some builders

---
+++ src/builders/EndpointBuilder.java
@@ -0,0 +1,50 @@
+package builders;
+
+import java.util.Set;
+
+import implementations.EndpointImpl;
+import interfaces.Endpoint;
+import interfaces.Method;
+
+public class EndpointBuilder {
+
+    String path;            // eg. "prices"
+    String label;           // eg. "endpoint for prices"
+
+    Set<String> docs;       // notes for using this endpoint
+    Set<Method> methods;    // endpoint 1-N methods
+
+    public EndpointBuilder path(String path) {
+
+        // validate path
+        this.path = path;
+        return this;
+    }
+
+    public EndpointBuilder label(String label) {
+
+        // validate label
+        this.label = label;
+        return this;
+    }
+
+    public EndpointBuilder docs(Set<String> docs) {
+
+        // validate docs
+        this.docs = docs;
+        return this;
+    }
+
+    public EndpointBuilder methods(Set<Method> methods) {
+
+        // validate methods
+        this.methods = methods;
+        return this;
+    }
+
+    public Endpoint build() {
+        return new EndpointImpl(path, label, docs, methods);
+    }
+
+
+}
