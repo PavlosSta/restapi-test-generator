e803a09ddf5935defe40e8e48946148814b0eab6
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-04-27 20:57:04 +0300

    Added test main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,72 @@
+import implementations.*;
+import interfaces.*;
+
+import java.util.function.Predicate;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
 
+		APISpecBuilder newApiBuilder 				= new APISpecBuilder();
+		EndpointSpecBuilder newEndpointBuilder		= new EndpointSpecBuilder();
+		HeaderSpecBuilder newHeaderBuilder 			= new HeaderSpecBuilder();
+		MethodSpecBuilder newMethodBuilder 			= new MethodSpecBuilder();
+		QueryParamSpecBuilder newQueryParamBuilder 	= new QueryParamSpecBuilder();
+		RequestSpecBuilder newRequestBuilder 		= new RequestSpecBuilder();
+		ResponseSpecBuilder newResponseBuilder		= new ResponseSpecBuilder();
+		StatusSpecBuilder newStatusBuilder			= new StatusSpecBuilder();
+
+		Predicate<String> statusCode = st -> (st.equals(200));
+
+		StatusSpec newStatus = newStatusBuilder
+				.setLabel("statusLabel")
+				.setBody("statusBody")
+				.setConditionBody(statusCode)
+				.build();
+
+		HeaderSpec newHeader = newHeaderBuilder
+				.setName("headerName")
+				.setBody("headerBody")
+				.setDefaultBody("headerDefBody")
+				.setMandatory(true)
+				.build();
+
+		QueryParamSpec newQueryParam = newQueryParamBuilder
+				.setName("queryName")
+				.setType("queryType")
+				.setBody("queryBody")
+				.setDefaultBody("queryDefBody")
+				.setMandatory(true)
+				.build();
+
+		RequestSpec newRequest = newRequestBuilder.
+				addHeaders(newHeader).
+				addQueryParams(newQueryParam).
+				setJwt("jwt").
+				build();
+
+		ResponseSpec newResponse = newResponseBuilder
+				.addHeaders(newHeader)
+				.addStatuses(newStatus)
+				.build();
+
+		MethodSpec newMethod = newMethodBuilder
+				.setType("")
+				.setRequest(newRequest)
+				.setResponse(newResponse)
+				.build();
+
+		EndpointSpec newEndpoint = newEndpointBuilder
+				.setPath("/test")
+				.setLabel("test endpoint")
+				.addMethods(newMethod)
+				.build();
+
+		APISpec newAPI = newApiBuilder
+				.setLabel("")
+				.setBaseUrl("/api")
+				.addEndpoints(newEndpoint)
+				.build();
 	}
 }
