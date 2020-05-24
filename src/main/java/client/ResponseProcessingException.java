bc683a7cf1ffd5d3ea49a5c629527bb98058ac8c
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-05-24 15:13:07 +0300

    Experimenting with FreeMarker

---
+++ src/main/java/client/ResponseProcessingException.java
@@ -0,0 +1,15 @@
+package client;
+
+public class ResponseProcessingException extends RuntimeException {
+
+    public ResponseProcessingException(String message) {
+
+        super(message);
+    }
+
+    public ResponseProcessingException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+
+}
