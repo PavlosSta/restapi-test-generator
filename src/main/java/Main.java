7f336266108158786cb9e11d6ac343d6ffaa081f
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-05-24 15:13:07 +0300

    Experimenting with FreeMarker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
+import freemarker.template.*;
 import implementations.*;
 import interfaces.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-
-		//TODO java template engines!
 
 		APISpecBuilder newApiBuilder 				= new APISpecBuilder();
 		EndpointSpecBuilder newEndpointBuilder		= new EndpointSpecBuilder();
@@ -61,10 +65,61 @@ public class Main
 
 		APISpec newAPI = newApiBuilder
 				.setLabel("api")
-				.setBaseUrl("/api")
+				.setBaseUrl("https://www.myapi.gr")
 				.addEndpoint(newEndpoint)
 				.build();
 
 		System.out.println(newAPI.getBaseUrl());
+
+
+		// Creates a Configuration instance
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
+
+		// Specifies the source where the template files come from.
+		try {
+			cfg.setDirectoryForTemplateLoading(new File("src/main/java/client"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		cfg.setDefaultEncoding("UTF-8");
+
+		// Sets how errors will appear.
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+		// Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+		cfg.setLogTemplateExceptions(false);
+
+		// Wrap unchecked exceptions thrown during template processing into TemplateException-s:
+		cfg.setWrapUncheckedExceptions(true);
+
+		// Do not fall back to higher scopes when reading a null loop variable:
+		cfg.setFallbackOnNullLoopVariable(false);
+
+		// Gets the template
+		try {
+			System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+			Template temp = cfg.getTemplate("restapi.ftlh");
+
+			// Prints to console
+			Writer out = new OutputStreamWriter(System.out);
+			temp.process(newAPI, out);
+
+		} catch (IOException | TemplateException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			File myObj = new File("filename.txt");
+			if (myObj.createNewFile()) {
+				System.out.println("File created: " + myObj.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
 	}
 }
---
+++ src/main/java/client/ConnectionException.java
@@ -0,0 +1,15 @@
+package client;
+
+public class ConnectionException extends RuntimeException {
+
+    public ConnectionException(String message) {
+
+        super(message);
+    }
+
+    public ConnectionException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+
+}
