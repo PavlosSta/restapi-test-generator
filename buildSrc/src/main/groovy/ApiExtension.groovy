3d1a1e5088e01aa95f6a0865f1af27339edf13e0
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-11-23 21:53:40 +0200

    Shiny new fluent groovy builder!

--- buildSrc/src/main/groovy/ApiExtension.groovy
+++ buildSrc/src/main/groovy/ApiExtension.groovy
@@ -1,7 +1,15 @@
+import org.pavlos.restapispec.client.generator.FreeMarkerJavaCodeGenerator
+import org.pavlos.restapispec.implementations.APISpecBuilder
+import org.pavlos.restapispec.implementations.EndpointSpecBuilder
+import org.pavlos.restapispec.implementations.HeaderSpecBuilder
+import org.pavlos.restapispec.implementations.MethodSpecBuilder
+import org.pavlos.restapispec.implementations.ParamSpecBuilder
+import org.pavlos.restapispec.implementations.RequestJSONSpecBuilder
+import org.pavlos.restapispec.implementations.RequestURLSpecBuilder
+import org.pavlos.restapispec.implementations.ResponseSpecBuilder
+import org.pavlos.restapispec.implementations.StatusSpecBuilder
+
 class ApiExtension {
-    String baseUrl
-    String label
-    List<Endpoint> endpoints
 
     String clientFolder
     String clientPackage
@@ -16,4 +24,186 @@ class ApiExtension {
     String mockName
 
     String serverPort
+
+    APISpecBuilder apiBuilder = new APISpecBuilder()
+    private EndpointSpecBuilder endpointBuilder
+    private HeaderSpecBuilder headerBuilder
+    private MethodSpecBuilder methodBuilder
+    private ParamSpecBuilder parameterBuilder
+    private RequestJSONSpecBuilder requestJSONBuilder
+    private RequestURLSpecBuilder requestURLBuilder
+    private ResponseSpecBuilder responseBuilder
+    private StatusSpecBuilder statusBuilder
+
+    private String whoCalls
+
+    void baseUrl(String baseUrl) {
+        apiBuilder.setBaseUrl(baseUrl)
+    }
+
+    void label(String label) {
+        apiBuilder.setLabel(label)
+    }
+
+    void endpoint(String path, Closure configuration) {
+        endpointBuilder = new EndpointSpecBuilder()
+        endpointBuilder.setPath(path)
+        configuration.call(this)
+        apiBuilder.addEndpoint(endpointBuilder.build())
+    }
+
+    void endpoint(String path, String attribute, Closure configuration) {
+        endpointBuilder = new EndpointSpecBuilder()
+        endpointBuilder.setPath(path)
+        endpointBuilder.addAttribute(attribute)
+        configuration.call(this)
+        apiBuilder.addEndpoint(endpointBuilder.build())
+    }
+
+    void endpointLabel(String label) {
+        endpointBuilder.setLabel(label)
+    }
+
+    void description(String description) {
+        endpointBuilder.addDescription(description)
+    }
+
+    void method(String type, Closure configuration) {
+        methodBuilder = new MethodSpecBuilder()
+        methodBuilder.setType(type)
+        configuration.call(this)
+        endpointBuilder.addMethod(methodBuilder.build())
+    }
+
+    void request(String type, Closure configuration) {
+        if (type == "JSON") {
+            whoCalls = "reqJSON"
+            requestJSONBuilder = new RequestJSONSpecBuilder()
+            configuration.call(this)
+            methodBuilder.setRequest(requestJSONBuilder.build())
+        }
+        else if (type == "URL") {
+            whoCalls = "reqURL"
+            requestURLBuilder = new RequestURLSpecBuilder()
+            configuration.call(this)
+            methodBuilder.setRequest(requestURLBuilder.build())
+        }
+    }
+
+    void response(String schema, Closure configuration) {
+        whoCalls = "response"
+        responseBuilder = new ResponseSpecBuilder()
+        responseBuilder.addResponseBodySchema(schema)
+        configuration.call(this)
+        methodBuilder.setResponse(responseBuilder.build())
+    }
+
+    void withBodyParameter(String name, String type) {
+        parameterBuilder = new ParamSpecBuilder()
+        parameterBuilder.setName(name)
+        parameterBuilder.setType(type)
+        parameterBuilder.setMandatory(true)
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addBodyParam(parameterBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addBodyParam(parameterBuilder.build())
+        }
+
+    }
+
+    void withBodyParameter(String name, String type, Object defaultValueIfOptionalAndMissing) {
+        parameterBuilder = new ParamSpecBuilder()
+        parameterBuilder.setName(name)
+        parameterBuilder.setType(type)
+        parameterBuilder.setMandatory(false)
+        parameterBuilder.setDefaultValue(defaultValueIfOptionalAndMissing.toString())
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addBodyParam(parameterBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addBodyParam(parameterBuilder.build())
+        }
+
+    }
+
+    void withQueryParameter(String name, String type) {
+        parameterBuilder = new ParamSpecBuilder()
+        parameterBuilder.setName(name)
+        parameterBuilder.setType(type)
+        parameterBuilder.setMandatory(true)
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addQueryParam(parameterBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addQueryParam(parameterBuilder.build())
+        }
+
+    }
+
+    void withQueryParameter(String name, String type, Object defaultValueIfOptionalAndMissing) {
+        parameterBuilder = new ParamSpecBuilder()
+        parameterBuilder.setName(name)
+        parameterBuilder.setType(type)
+        parameterBuilder.setMandatory(false)
+        parameterBuilder.setDefaultValue(defaultValueIfOptionalAndMissing.toString())
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addQueryParam(parameterBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addQueryParam(parameterBuilder.build())
+        }
+
+    }
+
+    void withHeader(String name) {
+        headerBuilder = new HeaderSpecBuilder()
+        headerBuilder.setName(name)
+        headerBuilder.setMandatory(true)
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addHeader(headerBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addHeader(headerBuilder.build())
+        }
+        else if (whoCalls == "response") {
+            responseBuilder.addHeader(headerBuilder.build())
+        }
+
+    }
+
+    void withHeader(String name, String defaultValueIfOptionalAndMissing) {
+        headerBuilder = new HeaderSpecBuilder()
+        headerBuilder.setName(name)
+        headerBuilder.setMandatory(false)
+        headerBuilder.setDefaultValueIfOptionalAndMissing(defaultValueIfOptionalAndMissing)
+
+        if (whoCalls == "reqURL") {
+            requestURLBuilder.addHeader(headerBuilder.build())
+        }
+        else if (whoCalls == "reqJSON") {
+            requestJSONBuilder.addHeader(headerBuilder.build())
+        }
+        else if (whoCalls == "response") {
+            responseBuilder.addHeader(headerBuilder.build())
+        }
+
+    }
+
+    void withStatus(Integer code, Closure configuration) {
+        statusBuilder = new StatusSpecBuilder()
+        statusBuilder.setCode(code.toString())
+        configuration.call(this)
+        responseBuilder.addStatus(statusBuilder.build())
+    }
+
+    void body(String body) {
+        statusBuilder.setBody(body)
+    }
+
 }
--- buildSrc/src/main/groovy/GroovyApiSpecBuilder.groovy
+++ buildSrc/src/main/groovy/GroovyApiSpecBuilder.groovy
@@ -3,8 +3,6 @@ import freemarker.template.TemplateExceptionHandler
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.pavlos.restapispec.client.generator.FreeMarkerJavaCodeGenerator
-import org.pavlos.restapispec.implementations.*
-import org.pavlos.restapispec.interfaces.*
 
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -12,142 +10,21 @@ import java.nio.file.Paths
 class GroovyApiSpecBuilder implements Plugin<Project> {
 
     void apply(Project project) {
+
         def apiExtension = project.extensions.create('api', ApiExtension)
 
         project.task('generate') {
-            doLast {
-                api {apiBuilder}
-                api_baseUrl "$apiExtension.baseUrl"
-                api_label "$apiExtension.label"
 
-                apiExtension.endpoints.each {
-
-                    endpoint { endpointBuilder }
-                    endpoint_path(it.path)
-                    endpoint_label(it.label)
-                    endpoint_description(it.description)
-
-                    if (it.attribute) {
-                        endpoint_attribute(it.attribute)
-                    }
-
-                    it.methods.each {
-
-                        method { methodBuilder }
-                        method_type(it.type)
-
-                        if (it.requestJSON) {
-                            requestJSON { requestJSONBuilder }
-
-                            it.requestJSON.bodyParameters.each {
-                                parameter { parameterBuilder }
-                                parameter_name(it.name)
-                                parameter_type(it.type)
-                                parameter_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    parameter_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestJSON_bodyParameter(parameter_build { parameterBuilder })
-                            }
-
-                            it.requestJSON.headers.each {
-                                header { headerBuilder }
-                                header_name(it.name)
-                                header_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    header_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestJSON_header(header_build { headerBuilder })
-                            }
-
-                            it.requestJSON.queryParameters.each {
-                                parameter { parameterBuilder }
-                                parameter_name(it.name)
-                                parameter_type(it.type)
-                                parameter_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    parameter_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestJSON_queryParameter(parameter_build { parameterBuilder })
-                            }
-
-                            method_request(requestJSON_build { requestJSONBuilder })
-
-                        }
-                        else if (it.requestURL) {
-                            requestURL { requestURLBuilder }
-
-                            it.requestURL.bodyParameters.each {
-                                parameter { parameterBuilder }
-                                parameter_name(it.name)
-                                parameter_type(it.type)
-                                parameter_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    parameter_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestURL_bodyParameter(parameter_build { parameterBuilder })
-                            }
-
-                            it.requestURL.headers.each {
-                                header { headerBuilder }
-                                header_name(it.name)
-                                header_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    header_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestURL_header(header_build { headerBuilder })
-                            }
-
-                            it.requestURL.queryParameters.each {
-                                parameter { parameterBuilder }
-                                parameter_name(it.name)
-                                parameter_type(it.type)
-                                parameter_mandatory(it.mandatory)
-                                if (!it.mandatory) {
-                                    parameter_defaultValue(it.defaultValueIfOptionalAndMissing)
-                                }
-                                requestURL_queryParameter(parameter_build { parameterBuilder })
-                            }
-
-                            method_request(requestURL_build { requestURLBuilder })
-                    
-                        }
-
-                        response {responseBuilder}
-
-                        response_schema(it.response.schema)
-
-                        status {statusBuilder}
-                        status_code(it.response.status.code)
-                        status_body(it.response.status.body)
-                        response_status(status_build {statusBuilder})
-
-                        it.response.headers.each {
-                            header { headerBuilder }
-                            header_name(it.name)
-                            header_mandatory(it.mandatory)
-                            if (!it.mandatory) {
-                                header_defaultValue(it.defaultValueIfOptionalAndMissing)
-                            }
-                            response_header(header_build { headerBuilder })
-                        }
-
-                        method_response(response_build {responseBuilder})
-
-                        endpoint_method(method_build {methodBuilder})
-
-                    }
-                    api_endpoint ( endpoint_build {endpointBuilder} )
-                }
+            doFirst {
 
                 /* Generate Rest API Client and Tests */
 
                 String projectPath = System.getProperty("user.dir")
 
                 // Creates a Configuration instance
-                cfg = new Configuration(Configuration.VERSION_2_3_30);
+                cfg = new Configuration(Configuration.VERSION_2_3_30)
 
-                cfg.setClassForTemplateLoading(FreeMarkerJavaCodeGenerator.class, "templates");
+                cfg.setClassForTemplateLoading(FreeMarkerJavaCodeGenerator.class, "templates")
 
                 // Specifies the source where the template files come from.
                 try {
@@ -155,21 +32,21 @@ class GroovyApiSpecBuilder implements Plugin<Project> {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                cfg.setDefaultEncoding("UTF-8");
+                cfg.setDefaultEncoding("UTF-8")
 
                 // Sets how errors will appear.
-                cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+                cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER)
 
                 // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
-                cfg.setLogTemplateExceptions(false);
+                cfg.setLogTemplateExceptions(false)
 
                 // Wrap unchecked exceptions thrown during template processing into TemplateException-s:
-                cfg.setWrapUncheckedExceptions(true);
+                cfg.setWrapUncheckedExceptions(true)
 
                 // Do not fall back to higher scopes when reading a null loop variable:
-                cfg.setFallbackOnNullLoopVariable(false);
+                cfg.setFallbackOnNullLoopVariable(false)
 
-                javaGenerator = new FreeMarkerJavaCodeGenerator(build {apiBuilder}, cfg)
+                javaGenerator = new FreeMarkerJavaCodeGenerator(apiExtension.apiBuilder.build(), cfg)
 
                 String clientFolder = "$apiExtension.clientFolder"
                 String clientPackage = "$apiExtension.clientPackage"
@@ -208,232 +85,8 @@ class GroovyApiSpecBuilder implements Plugin<Project> {
         }
     }
 
-    // Builders
-    private APISpecBuilder apiBuilder
-    private EndpointSpecBuilder endpointBuilder
-    private HeaderSpecBuilder headerBuilder
-    private MethodSpecBuilder methodBuilder
-    private ParamSpecBuilder parameterBuilder
-    private RequestJSONSpecBuilder requestJSONBuilder
-    private RequestURLSpecBuilder requestURLBuilder
-    private ResponseSpecBuilder responseBuilder
-    private StatusSpecBuilder statusBuilder
+    // Freemarker Generator
     private FreeMarkerJavaCodeGenerator javaGenerator
-
-    // Freemarker Configuration
     private Configuration cfg
-
-    // API
-    void api(Closure c) {
-        apiBuilder = new APISpecBuilder()
-        c.call(this)
-    }
-
-    void api_baseUrl(String url) {
-        apiBuilder.setBaseUrl(url);
-    }
-
-    void api_label(String label) {
-        apiBuilder.setLabel(label);
-    }
-
-    void api_endpoint(EndpointSpec endpoint) {
-        apiBuilder.addEndpoint(endpoint)
-    }
-
-    // Endpoint
-    void endpoint(Closure c) {
-        endpointBuilder = new EndpointSpecBuilder()
-        c.call(this)
-    }
-
-    void endpoint_path(String path) {
-        endpointBuilder.setPath(path)
-    }
-
-    void endpoint_label(String label) {
-        endpointBuilder.setLabel(label)
-    }
-
-    void endpoint_description(String description) {
-        endpointBuilder.addDescription(description)
-    }
-
-    void endpoint_attribute(String attribute) {
-        endpointBuilder.addAttribute(attribute)
-    }
-
-    void endpoint_method(MethodSpec method) {
-        endpointBuilder.addMethod(method)
-    }
-
-    // Method
-    void method(Closure c) {
-        methodBuilder = new MethodSpecBuilder()
-        c.call(this)
-    }
-
-    void method_type(String type) {
-        methodBuilder.setType(type)
-    }
-
-    void method_request(RequestSpec request) {
-        methodBuilder.setRequest(request)
-    }
-
-    void method_response(ResponseSpec response) {
-        methodBuilder.setResponse(response)
-    }
-
-    // Header
-    void header(Closure c) {
-        headerBuilder = new HeaderSpecBuilder()
-        c.call(this)
-    }
-
-    void header_name(String name) {
-        headerBuilder.setName(name)
-    }
-
-    void header_mandatory(boolean mandatory) {
-        headerBuilder.setMandatory(mandatory)
-    }
-
-    void header_defaultValue(String defaultValue) {
-        headerBuilder.setDefaultValueIfOptionalAndMissing(defaultValue)
-    }
-
-    // RequestJSON
-    void requestJSON(Closure c) {
-        requestJSONBuilder = new RequestJSONSpecBuilder()
-        c.call(this)
-    }
-
-    void requestJSON_bodyParameter(ParameterSpec bodyParam) {
-        requestJSONBuilder.addBodyParam(bodyParam)
-    }
-
-    void requestJSON_header(HeaderSpec header) {
-        requestJSONBuilder.addHeader(header)
-    }
-
-    void requestJSON_queryParameter(ParameterSpec queryParameter) {
-        requestJSONBuilder.addQueryParam(queryParameter)
-    }
-
-    // RequestURL
-    void requestURL(Closure c) {
-        requestURLBuilder = new RequestURLSpecBuilder()
-        c.call(this)
-    }
-
-    void requestURL_bodyParameter(ParameterSpec bodyParam) {
-        requestURLBuilder.addBodyParam(bodyParam)
-    }
-
-    void requestURL_header(HeaderSpec header) {
-        requestURLBuilder.addHeader(header)
-    }
-
-    void requestURL_queryParameter(ParameterSpec bodyParam) {
-        requestURLBuilder.addQueryParam(bodyParam)
-    }
-
-    // Response
-    void response(Closure c) {
-        responseBuilder = new ResponseSpecBuilder()
-        c.call(this)
-    }
-
-    void response_schema(String schema) {
-        responseBuilder.addResponseBodySchema(schema)
-    }
-
-    void response_status(StatusSpec status) {
-        responseBuilder.addStatus(status)
-    }
-
-    void response_header(HeaderSpec header) {
-        responseBuilder.addHeader(header)
-    }
-
-    // Parameter
-    void parameter(Closure c) {
-        parameterBuilder = new ParamSpecBuilder()
-        c.call(this)
-    }
-
-    void parameter_name(String name) {
-        parameterBuilder.setName(name)
-    }
-
-    void parameter_type(String type) {
-        parameterBuilder.setType(type)
-    }
-
-    void parameter_mandatory(boolean mandatory) {
-        parameterBuilder.setMandatory(mandatory)
-    }
-
-    void parameter_defaultValue(String defaultValue) {
-        parameterBuilder.setDefaultValue(defaultValue)
-    }
-
-    // Status
-    void status(Closure c) {
-        statusBuilder = new StatusSpecBuilder()
-        c.call(this)
-    }
-
-    void status_code(String code) {
-        statusBuilder.setCode(code)
-    }
-
-    void status_body(String body) {
-        statusBuilder.setBody(body)
-    }
-
-    // Misc
-    void methodMissing(String name) {
-
-    }
-
-    // Build Specs
-
-    APISpec build(Closure c) {
-        apiBuilder.build()
-    }
-
-    EndpointSpec endpoint_build(Closure c) {
-        endpointBuilder.build()
-    }
-
-    MethodSpec method_build(Closure c) {
-        methodBuilder.build()
-    }
-
-    HeaderSpec header_build(Closure c) {
-        headerBuilder.build()
-    }
-
-    ParameterSpec parameter_build(Closure c) {
-        parameterBuilder.build()
-    }
-
-    RequestJSONSpec requestJSON_build(Closure c) {
-        requestJSONBuilder.build()
-    }
-
-    RequestURLSpec requestURL_build(Closure c) {
-        requestURLBuilder.build()
-    }
-
-    ResponseSpec response_build(Closure c) {
-        responseBuilder.build()
-    }
-
-    StatusSpec status_build(Closure c) {
-        statusBuilder.build()
-    }
 
 }
