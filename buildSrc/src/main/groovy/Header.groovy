d9539abbebf853f04dd6f88a875ea2b2f8564132
Pavlos Stathopoulos <a3r1k0@gmail.com>
2020-11-23 21:59:32 +0200

    Cleanup

--- buildSrc/src/main/groovy/Header.groovy
+++
@@ -1,5 +0,0 @@
-class Header {
-    String name
-    String defaultValueIfOptionalAndMissing
-    boolean mandatory
-}
--- buildSrc/src/main/groovy/RequestURL.groovy
+++
@@ -1,6 +0,0 @@
-class RequestURL {
-    List<Parameter> bodyParameters
-    List<Header> headers
-    List<Parameter> queryParameters
-
-}
